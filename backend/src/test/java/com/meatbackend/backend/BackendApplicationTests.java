6089e84f9c77240a22cdfa0874d785e1aae9e1e9
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-06-17 03:50:00 +0530

    frontnd commit

---
+++ backend/src/test/java/com/meatbackend/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meatbackend.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
