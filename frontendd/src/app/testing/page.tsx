252b0871fdb485f63e523a248a54b340a311892c
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-06-17 23:51:53 +0530

    react-redux

--- frontendd/src/app/testing/page.tsx
+++ frontendd/src/app/testing/page.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import { Login } from '../Componemts/Login'
 
 
 const page = () => {
   return (
     <div className=''>
-        {/* <SignUp/> */}
-
-        {/* <DisplayProducts/> */}
-        <Login/>
+       
+   
       
     </div>
   )
